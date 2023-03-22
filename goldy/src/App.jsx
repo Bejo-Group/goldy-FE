cd8fa21b58eef5bc40b9a7b1f1cfa256afde5864
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-03-23 04:40:36 +0700

    Add component Navbar

--- goldy/src/App.jsx
+++ goldy/src/App.jsx
@@ -1,17 +1,18 @@
 import DatePicker from './components/DatePicker'
-import "./index.css"
-// import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar"
 import OutputHarga from './components/OutputHarga'
+import "./index.css"
 
 function App() {
   return (
-    <div>
-      {/* <Navbar/> */}
-       <h1 className="text-3xl font-bold underline text-gold">Hello world!</h1>
-       <DatePicker></DatePicker>
-       <OutputHarga></OutputHarga>
-
-    </div>
+    <>
+      <Navbar/>
+      <div>
+        <h1 className="text-3xl font-bold underline text-gold">Hello world!</h1>
+        <DatePicker></DatePicker>
+        <OutputHarga></OutputHarga>
+      </div>
+    </>
   );
 }
 
