a799f1ebd3521305829c1359dd22cddd45f82ca5
Vallentina Wahyu Febrihartanti <vallentina.wahyu15@gmail.com>
2023-03-22 21:33:45 +0700

    Add DatePicker

--- goldy/src/App.jsx
+++ goldy/src/App.jsx
@@ -1,13 +1,11 @@
-
-import DatePicker from './components/DatePicker.js'
+import DatePicker from './components/DatePicker.jsx'
 import "./index.css"
-import Navbar from "./components/Navbar"
-
+// import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      {/* <Navbar/> */}
        <h1 className="text-3xl font-bold underline text-gold">Hello world!</h1>
        <DatePicker></DatePicker>
        
