ea5031545cce82156b312504eaf35934090d8bbc
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-03-22 17:40:28 +0700

    Add library react-router-dom

--- goldy/src/App.jsx
+++ goldy/src/App.jsx
@@ -1,8 +1,10 @@
 import "./index.css"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div>
+      <Navbar/>
        <h1 className="text-3xl font-bold underline text-gold">
           Hello world!
       </h1>
