13c2341f2064fb30f87eb635120f790432fff214
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-03-22 08:41:45 +0700

    Set up tailwindcss screen

--- goldy/tailwind.config.js
+++ goldy/tailwind.config.js
@@ -4,20 +4,39 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        black: '#0B0C0C',
-        darkgray: '#2A2E34',
-        lightgray: '#E9EAEC',
-        yellow: '#FBE134',
-        gold: '#E4B61A',
-        white: '#FFFFFF',
+        transparent: "transparent",
+        current: "currentColor",
+        black: "#0B0C0C",
+        darkgray: "#2A2E34",
+        lightgray: "#E9EAEC",
+        yellow: "#FBE134",
+        gold: "#E4B61A",
+        white: "#FFFFFF",
       },
     },
     fontFamily: {
-      primary: ['Source Sans Pro'], 
-      secondary: ['Poppins']
-    }
+      primary: ["Source Sans Pro"],
+      secondary: ["Poppins"],
+    },
+    screens: {
+      xm: "350px",
+      // => @media (min-width: 350px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "913px",
+      // => @media (min-width: 913px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
-}
+};
