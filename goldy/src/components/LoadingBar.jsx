2f982d64a28c30ab511869deb91949beb0f1c700
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-03-23 08:04:42 +0700

    Modify component button

--- goldy/src/components/LoadingBar.jsx
+++ goldy/src/components/LoadingBar.jsx
@@ -8,7 +8,9 @@ export default function LoadingBar(){
 
   return (
     <div>
-      <button onClick={notify}>Test!</button>
+      <button onClick={notify} className="bg-black hover:bg-darkgray text-white font-bold py-1 px-10 rounded-full">
+        Tekan Aku, Ayo !!!
+      </button>
       <ToastContainer />
     </div>
   );
