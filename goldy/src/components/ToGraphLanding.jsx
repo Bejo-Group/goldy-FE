1b89be1f742805b67666e38f1a15fff10be63578
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-04-30 18:34:06 +0700

    Modify graph component

--- goldy/src/components/ToGraphLanding.jsx
+++ goldy/src/components/ToGraphLanding.jsx
@@ -14,14 +14,18 @@ function ToGraphLanding () {
                 alt="Dummy-Graph" 
                 />
                 <div className="px-4">
-                    <p>Harga Terakhir</p>
-                    <p>Rp1.049.000,00</p>
+                    <div className="pb-2">
+                        <p>Harga Terakhir</p>
+                        <p className="text-gold text-h-md">Rp1.049.000,00</p>
+                    </div>
                     
-                    <p>Perubahan</p>
-                    <p>Rp0.00</p>
+                    <div className="pb-2">
+                        <p >Perubahan</p>
+                        <p className="text-gold text-h-md">Rp0.00</p>
+                    </div>
 
                     <p>Perubahan Terakhir</p>
-                    <p>12 Maret 2023 06:46:21</p>
+                    <p className="text-gold text-h-md">12 Maret 2023 06:46:21</p>
                 </div>
             </div>
         </div>  
