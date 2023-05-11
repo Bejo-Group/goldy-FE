4dbd27936c88eee7d1672d6af186f427c9336836
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-05-11 22:45:25 +0700

    Modify navbar menus

--- goldy/src/components/Navbar.jsx
+++ goldy/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Nav() {
   
   return (
     <div>
-      <nav className="bg-gold font-primary font-bold text-2xl w-full">
+      <nav className="bg-gold font-primary font-bold text-h-sm w-full">
         <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-24">
             <div className="flex items-center">
