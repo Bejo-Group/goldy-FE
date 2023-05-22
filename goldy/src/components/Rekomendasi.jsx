a35f16aa372405643f30f5e09c6d77b4ba9841ce
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-05-22 14:21:04 +0700

    Fix footer on rekomendasi and prediksi page

--- goldy/src/components/Rekomendasi.jsx
+++ goldy/src/components/Rekomendasi.jsx
@@ -6,13 +6,15 @@ import DummyGraph from './ToGraphLanding'
 function Rekomendasi(){
     return(
         <>
-            <Navbar/>
-            <DummyGraph/>
-            <div className='flex'>
-                <div>kiri</div>
-                <div>kanan</div>
+            <div className='flex flex-col h-screen'>
+                <Navbar/>
+                <DummyGraph/>
+                <div className='flex'>
+                    <div>kiri</div>
+                    <div>kanan</div>
+                </div>
+                <Footer/>   
             </div>
-            <Footer/>   
         </>
     )
 }
