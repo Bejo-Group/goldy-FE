df33e86788499ec6cbee920e053c3677ee2c7be0
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-06-06 10:40:22 +0700

    Update Prediksi.jsx

--- goldy/src/components/Prediksi.jsx
+++ goldy/src/components/Prediksi.jsx
@@ -9,22 +9,22 @@ import axios from 'axios';
 import {toast} from 'react-toastify';
 
 function Prediksi(){
-    const [SPX, setSPX] = useState(0);
-    const[USO, setUSO]  = useState(0);
-    const[SLV, setSLV] = useState(0);
-    const[EUR_USD, setEUR_USD] = useState(0);
+    const [StockMarket_SPX, setSPX] = useState(0);
+    const[StockMarket_USO, setUSO]  = useState(0);
+    const[StockMarket_SLV, setSLV] = useState(0);
+    const[StockMarket_EUR_USD, setEUR_USD] = useState(0);
     const [prediction, setPrediction] = useState();
     const onSubmit = async () => {
-        if (SPX === "" || USO === "" || SLV === "" || EUR_USD === "") {
+        if (StockMarket_SPX === "" || StockMarket_USO === "" || StockMarket_SLV === "" || StockMarket_EUR_USD === "") {
             toast.error("All field must be filled!");
             console.log("All field must be filled!")
         } else {
             axios.get("http://127.0.0.1:5000/prediction", {
                 params: {
-                    SPX: SPX,
-                    USO: USO,
-                    SLV: SLV,
-                    EUR_USD: EUR_USD,
+                    SPX: StockMarket_SPX,
+                    USO: StockMarket_USO,
+                    SLV: StockMarket_SLV,
+                    EUR_USD: StockMarket_EUR_USD,
                 },
         })
         .then((response) => {
@@ -48,19 +48,73 @@ function Prediksi(){
                 <div className="row flex">
                     <div className="column flex-[50%]">
                         <div className='pl-60'>
-                            <div className='flex'>
-                                <div className='text-secondary text-h-md pr-5'>Tanggal :</div>   
-                                <input class="shadow appearance-none border rounded w-3/5 py-2 px-3 text-darkgray leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Masukkan tanggal kapan harga diprediksi ..."/>                                
+                            {/* SPX Section */}
+                            <div className='flex mt-10'>
+                                <div className='text-secondary text-h-md pr-5'>SPX :</div>   
+                                <input 
+                                className="shadow appearance-none border rounded w-3/5 py-2 px-3 text-darkgray leading-tight focus:outline-none focus:shadow-outline" 
+                                placeholder="Masukkan nilai SPX"
+                                type="number"
+                                required
+                                value={StockMarket_SPX} 
+                                onChange={(event) => setSPX(event.target.value)}
+                                />                                
                             </div>
+
+                            {/* USO Section */}
+                            <div className='flex mt-10'>
+                                <div className='text-secondary text-h-md pr-5'>USO :</div>   
+                                <input 
+                                className="shadow appearance-none border rounded w-3/5 py-2 px-3 text-darkgray leading-tight focus:outline-none focus:shadow-outline" 
+                                placeholder="Masukkan nilai USO"
+                                type="number"
+                                required
+                                value={StockMarket_USO}
+                                onChange={(event) => setUSO(event.target.value)} 
+                                />                                
+                            </div>
+
+                            {/* SLV Section */}
+                            <div className='flex mt-10'>
+                                <div className='text-secondary text-h-md pr-5'>SLV :</div>   
+                                <input 
+                                className="shadow appearance-none border rounded w-3/5 py-2 px-3 text-darkgray leading-tight focus:outline-none focus:shadow-outline" 
+                                placeholder="Masukkan nilai SLV"
+                                type="number"
+                                required
+                                value={StockMarket_SLV} 
+                                onChange={(event) => setSLV(event.target.value)}
+                                />                                
+                            </div>
+
+                            {/* EUS_USD Section */}
+                            <div className='flex mt-10'>
+                                <div className='text-secondary text-h-md pr-5'>EUR/USD :</div>   
+                                <input 
+                                className="shadow appearance-none border rounded w-3/5 py-2 px-3 text-darkgray leading-tight focus:outline-none focus:shadow-outline" 
+                                placeholder="Masukkan nilai EUR/USD"
+                                type="number"
+                                required
+                                value={StockMarket_EUR_USD}
+                                onChange={(event) => setEUR_USD(event.target.value)} 
+                                />                                
+                            </div>
+
                             <div>
                                 <Link to="">
-                                    <button className='text-white bg-black px-20 py-1 mt-6 justify-center rounded-2xl hover:bg-darkgray hover:text-lightgray'>Proses</button>
+                                    <button 
+                                    className='text-white bg-black px-20 py-1 mt-6 justify-center rounded-2xl hover:bg-darkgray hover:text-lightgray'
+                                    onClick={onSubmit}
+                                    type='button'
+                                    >
+                                        Proses
+                                    </button>
                                 </Link>     
                             </div>
                         </div>
                     </div>
-                    <div className="column flex-[50%] text-secondary text-h-md">
-                        Output
+                    <div className="result column flex-[50%] text-secondary text-h-md">
+                        <label>{prediction}</label>
                     </div>
                 </div>
                 <Footer/>   
